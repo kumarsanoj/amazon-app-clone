52c004c2e25d330e3e3b65316b89da45d1e68b18
sanoj kumar <kumarsanoj2101@gmail.com>
2021-08-08 17:18:46 +0530

    implemented home and product item component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Home.css';
 import Product from '../product/Product';
+import getFlatDataList from '../../stub/item'
 
 const Home = () => {
     return (
         <div className="home">
             <img className="home__image"
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" />
-            <Product id="1" title="product1" image="" price="150" rating={4}/>
-            <Product id="1" title="product1" image="" price="150" rating={4}/>
-            <Product id="1" title="product1" image="" price="150" rating={4}/>
+                <div className="home__productContainer">
+                {
+                getFlatDataList().map(({id, name, imageUrl, rating, price}) => (
+                    <Product id={id} name={name} image={imageUrl} price={price} rating={rating}/>  
+                ))
+            }
+                </div>
+            
         </div>
     )
 }
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import './Product.css'
-const Product = ({id, title, image, price, rating}) => {
+const Product = ({ name, image, price, rating}) => {
     return (
         <div className="product">
-            <p>{title}</p>
-            <p>
-                <small>$</small>
+            <p className="product__info">{name}</p>
+            <p className="product__price">
+                <small>₹</small>
                 <strong>{price}</strong>
             </p>
             <div className="product__rating">
                 {
-                    Array(rating).fill().map(
+                    Array(rating || 5).fill().map(
                         (_) => (
-                            <p>*</p>
+                            <p>⭐</p>
                         )
                     )
                 }
             </div>
-            <img src={image} alt="" />
+            <img className="product__image" src={image} alt="" />
             <button>Add to Basket</button>
         </div>
     )
